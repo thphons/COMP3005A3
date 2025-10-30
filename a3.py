2cabfc0881295274d43dd7c3ea2fab66513daaba
Cal McLelland <thphons@gmail.com>
2025-10-30 14:48:41 -0400

    added token parsing

--- a3.py
+++ a3.py
@@ -1,4 +1,8 @@
-# repl.py
+##  Cal McLelland
+#   Assignment 3
+##  COMP 3005 Fall 2025
+
+# a3.py
 import code
 
 ## define banner and exit messages
@@ -6,11 +10,26 @@ bannerMsg = "COMP3005 A3!"
 exitMsg = "bye!"
 
 
+# create a read-eval-print loop
 class Repl(code.InteractiveConsole):
     def runsource(self, source, filename="<input>", symbol="single"):
-        match source:
+        tokens = source.split()
+        command = tokens[0]
+        args = tokens.pop(0)
+        match command:
             case "help":
                 print("help incoming...")
+            case "listStudents":
+                print("listing all students...")
+            case "addStudent":
+                print("adding new student...")
+            case "updateStudentEmail":
+                print("updating student email")
+            case "deleteStudent":
+                print("deleting student")
+            case "exit" | "close" | "quit":
+                print(exitMsg)
+                exit(0)
             case _:
                 print("command not recognized:", source)
 
