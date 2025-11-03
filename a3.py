4fe7bf852dddfd35b723f01a1855020a95209789
Cal McLelland <thphons@gmail.com>
2025-11-03 13:17:49 -0500

    a3 added function stubs and help message

--- a3.py
+++ a3.py
@@ -8,6 +8,38 @@ import code
 ## define banner and exit messages
 bannerMsg = "COMP3005 A3!"
 exitMsg = "bye!"
+helpMsg =   "\nValid commands are:\n" \
+            "   help                                                    " \
+            "list all commands\n" \
+            "   listStudents                                            " \
+            "list all student records\n" \
+            "   addStudents <first_name> <last_name> <email> <dob>      " \
+            "add a new student record with the specified values\n" \
+            "   deleteStudent <student_id>                              " \
+            "delete the student record with the specified id\n" \
+            "   exit                                                    " \
+            "exit the program\n"
+
+def listStudents():
+    print("listing all students...")
+    #reteive and list all student records
+
+def addStudent(args):
+    print("adding new student...")
+    #check for valid arguments
+    #check if student already exists
+
+def updateStudentEmail(args):
+    print("updating student email...")
+    #check for valid arguments
+    #check if student exists
+    #update the students email
+
+def deleteStudent(args):
+    print("updating student email")
+    #check for valid arguments
+    #check if student exists
+    #delete student
 
 
 # create a read-eval-print loop
@@ -18,17 +50,16 @@ class Repl(code.InteractiveConsole):
         args = tokens.pop(0)
         match command:
             case "help":
-                print("help incoming...")
+                print(helpMsg)
             case "listStudents":
-                print("listing all students...")
+                listStudents()
             case "addStudent":
-                print("adding new student...")
+                addStudent(args)
             case "updateStudentEmail":
-                print("updating student email")
+                updateStudentEmail(args)
             case "deleteStudent":
-                print("deleting student")
-            case "exit" | "close" | "quit":
-                print(exitMsg)
+                deleteStudent(args)
+            case "exit" | "quit":
                 exit(0)
             case _:
                 print("command not recognized:", source)
